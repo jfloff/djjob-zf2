5ee2e19198baefe2f2da9c306101e2ecb0c8b814
João Ferreira Loff <jfloff@gmail.com>
2013-02-06 15:52:47 +0000

    classmap fix

--- autoload_classmap.php
+++ autoload_classmap.php
@@ -1,9 +1,9 @@
 <?php
 
 return array(
-	'DJJob' => __DIR__ . 'src/DJJob/DJJob.php',
-	'DJBase' => __DIR__ . 'src/DJJob/DJJob.php',
-	'DJException' => __DIR__ . 'src/DJJob/DJJob.php',
-	'DJWorker' => __DIR__ . 'src/DJJob/DJJob.php',
-	'DJRetryException' => __DIR__ . 'src/DJJob/DJJob.php',
+	'DJJob' => __DIR__ . '/src/DJJob/DJJob.php',
+	'DJBase' => __DIR__ . '/src/DJJob/DJJob.php',
+	'DJException' => __DIR__ . '/src/DJJob/DJJob.php',
+	'DJWorker' => __DIR__ . '/src/DJJob/DJJob.php',
+	'DJRetryException' => __DIR__ . '/src/DJJob/DJJob.php',
 );
